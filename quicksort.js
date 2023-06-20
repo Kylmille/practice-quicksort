50a70edfebc6c35f794095ed76c2a1691eacd4bb
Kyle <Kylesiphone625@gmail.com>
2023-06-20 19:39:59 -0400

    modified:   package-lock.json 	modified:   quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,8 +1,27 @@
 function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) {
+      continue;
+    }
+
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quicksort(left), pivot, ...quicksort(right)];
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
-
   // Pick the first value as the pivot
 
   // Orient the pivot so that...
